40ab88c795f932690ed990cebf5f03ad9159b1ff
Allan Clempe <allanclempe@users.noreply.github.com>
2021-03-26 10:35:39 +1000

    Enable non-security connection for dev environment (#17)

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,9 +1,9 @@
 class AppConfig {
   AppConfig() {
     params['dev'] = AppConfigParams(
-        "http://192.168.182.2:7546",
-        "ws://192.168.182.2:7546",
-        "0x59FFB6Ea7bb59DAa2aC480D862d375F49F73915d");
+        "http://192.168.40.197:8545",
+        "ws://192.168.40.197:8545",
+        "0xD933a953f4786Eed5E58D234dFeadE15c96bAa8b");
 
     params['ropsten'] = AppConfigParams(
         "https://ropsten.infura.io/v3/628074215a2449eb960b4fe9e95feb09",
