967b698d268bd7d189001768f91cae708f9aa0c7
Allan Clempe <allan.clempe@gmail.com>
2021-05-15 11:23:07 +1000

    removed ml_vision in favor of qr_code_scanner for simplicity

--- lib/qrcode_reader_page.dart
+++ lib/qrcode_reader_page.dart
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:etherwallet/utils/scanner_utils.dart';
-import 'package:camera/camera.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'utils/detector_painters.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 typedef OnScanned = void Function(String? address);
 
@@ -27,152 +26,80 @@ class QRCodeReaderPage extends StatefulWidget {
 }
 
 class _QRCodeReaderPageState extends State<QRCodeReaderPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
   static final RegExp _basicAddress =
       RegExp(r'^(0x)?[0-9a-f]{40}', caseSensitive: false);
-  List<Barcode>? _scanResults;
-  CameraController? _camera;
-  bool _isDetecting = false;
-  CameraLensDirection _direction = CameraLensDirection.back;
 
-  final BarcodeDetector _barcodeDetector =
-      FirebaseVision.instance.barcodeDetector();
+  StreamSubscription? _subscription;
 
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
   @override
-  void initState() {
-    super.initState();
-
-    _initializeCamera();
-  }
-
-  void _initializeCamera() async {
-    final CameraDescription description =
-        await ScannerUtils.getCamera(_direction);
-
-    _camera = CameraController(
-      description,
-      defaultTargetPlatform == TargetPlatform.iOS
-          ? ResolutionPreset.low
-          : ResolutionPreset.medium,
-      enableAudio: false,
-    );
-
-    await _camera?.initialize();
-
-    _camera?.startImageStream((CameraImage image) {
-      if (_isDetecting) return;
-
-      _isDetecting = true;
-
-      ScannerUtils.detect(
-        image: image,
-        detectInImage: _barcodeDetector.detectInImage,
-        imageRotation: description.sensorOrientation,
-      ).then((dynamic results) => results as List<Barcode>).then(
-        (List<Barcode> results) {
-          setState(() {
-            _scanResults = results
-                .where((item) =>
-                    item.displayValue != null &&
-                    _basicAddress.hasMatch(item.displayValue!))
-                .toList();
-          });
-        },
-      ).whenComplete(() {
-        if (_scanResults != null && _scanResults!.length > 0) {
-          if (widget.onScanned != null)
-            widget.onScanned!(_scanResults!.first.displayValue);
-
-          if (widget.closeWhenScanned) {
-            _isDetecting = true; // stop looping
-
-            Future.delayed(Duration(seconds: 1), () {
-              if (Navigator.canPop(context)) {
-                Navigator.pop(context);
-              }
-            });
-
-            return;
-          }
-        }
-        _isDetecting = false;
-      });
-    });
-  }
-
-  Widget _buildResults() {
-    if (_scanResults == null ||
-        _camera == null ||
-        !_camera!.value.isInitialized) {
-      return Container();
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller?.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller?.resumeCamera();
     }
-
-    if (_camera!.value.previewSize == null) return Container();
-
-    final Size imageSize = Size(
-      _camera!.value.previewSize!.height,
-      _camera!.value.previewSize!.width,
-    );
-
-    return CustomPaint(
-      painter: BarcodeDetectorPainter(imageSize, _scanResults!),
-    );
-  }
-
-  Widget _buildImage() {
-    return Container(
-      constraints: const BoxConstraints.expand(),
-      child: _camera == null
-          ? const Center(
-              child: Text('Initialising camera, please wait.'),
-            )
-          : Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                CameraPreview(_camera!),
-                _buildResults(),
-              ],
-            ),
-    );
-  }
-
-  void _toggleCameraDirection() async {
-    if (_direction == CameraLensDirection.back) {
-      _direction = CameraLensDirection.front;
-    } else {
-      _direction = CameraLensDirection.back;
-    }
-
-    await _camera?.stopImageStream();
-    await _camera?.dispose();
-
-    setState(() {
-      _camera = null;
-    });
-
-    _initializeCamera();
   }
 
   @override
   Widget build(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildImage(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _toggleCameraDirection,
-        child: _direction == CameraLensDirection.back
-            ? const Icon(Icons.camera_front)
-            : const Icon(Icons.camera_rear),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                  borderColor: Colors.red,
+                  borderRadius: 10,
+                  borderLength: 30,
+                  borderWidth: 10,
+                  cutOutSize: scanArea,
+                )),
+          ),
+        ],
       ),
     );
   }
 
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    _subscription = controller.scannedDataStream.listen((scanData) {
+      // metamask qrcode adds "ethereum:" in front of the address.
+      var address = scanData.code.replaceAll("ethereum:", "");
+
+      if (!_basicAddress.hasMatch(address)) {
+        return;
+      }
+
+      widget.onScanned!(address);
+
+      if (widget.closeWhenScanned) {
+        _subscription?.cancel();
+
+        if (Navigator.canPop(context)) {
+          Navigator.pop(context);
+        }
+      }
+    });
+  }
+
   @override
   void dispose() {
-    _camera?.dispose().then((_) {
-      _barcodeDetector.close();
-    });
+    controller?.dispose();
+    _subscription?.cancel();
 
     super.dispose();
   }
