280e628bcc5658ef7386b5a20db670b7b481788f
Allan Clempe <allan.clempe@gmail.com>
2023-03-09 17:15:49 +1000

    refactored address and balance components

--- lib/components/wallet/balance.dart
+++ lib/components/wallet/balance.dart
@@ -1,56 +1,28 @@
-import 'package:etherwallet/components/copyButton/copy_button.dart';
 import 'package:etherwallet/utils/eth_amount_formatter.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class Balance extends StatelessWidget {
   const Balance({
     Key? key,
-    required this.address,
-    required this.ethBalance,
-    required this.tokenBalance,
-    required this.symbol,
+    required this.balance,
+    this.symbol = '',
+    this.fontSizeDelta = 0,
+    this.fontColor = Colors.black,
   }) : super(key: key);
 
-  final String? address;
-  final BigInt? ethBalance;
-  final BigInt? tokenBalance;
+  final BigInt? balance;
   final String? symbol;
+  final double fontSizeDelta;
+  final Color fontColor;
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(address ?? ''),
-          const SizedBox(height: 10),
-          CopyButton(
-            text: const Text('Copy address'),
-            value: address,
-          ),
-          if (address != null &&
-              (mediaQuery.orientation == Orientation.portrait || kIsWeb))
-            QrImage(
-              data: address!,
-              size: 150.0,
-            ),
-          Text(
-            '${EthAmountFormatter(tokenBalance).format()} tokens',
-            style:
-                Theme.of(context).textTheme.bodyText2?.apply(fontSizeDelta: 6),
-          ),
-          Text(
-            '${EthAmountFormatter(ethBalance).format()} $symbol',
-            style: Theme.of(context)
-                .textTheme
-                .bodyText2
-                ?.apply(color: Colors.blueGrey),
-          )
-        ],
+    final theme = Theme.of(context);
+    return Text(
+      '${EthAmountFormatter(balance).format()} $symbol',
+      style: theme.textTheme.bodyLarge?.apply(
+        fontSizeDelta: fontSizeDelta,
+        color: fontColor,
       ),
     );
   }
